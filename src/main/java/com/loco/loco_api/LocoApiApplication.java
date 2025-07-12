8d5e16af88d66500f8ee12b11638c75c80007832
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-07-12 17:33:55 +0900

    chore: project init

---
+++ src/main/java/com/loco/loco_api/LocoApiApplication.java
@@ -0,0 +1,13 @@
+package com.loco.loco_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocoApiApplication.class, args);
+	}
+
+}
